981ce3f2cd865d9a80712adec52539c8cca6db1a
lqhandsome <1291752135@qq.com>
2021-05-24 22:14:19 +0800

    读取目录下的文件名

--- unredo/yuLiao/main.go
+++ unredo/yuLiao/main.go
@@ -1,5 +1,34 @@
 package main
 
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
+	"strings"
+)
+
 func main() {
-	$END$
+	pwd, _ := os.Getwd()
+	//获取文件或目录相关信息
+	fileInfoList, err := ioutil.ReadDir(pwd)
+	if err != nil {
+		log.Fatal(err)
+	}
+	writeFile := "图纸名.txt"
+	file, err := os.OpenFile(writeFile, os.O_APPEND|os.O_CREATE, 777)
+	defer file.Close()
+	if err != nil {
+		fmt.Println("err=", err)
+		return
+	}
+	for i := range fileInfoList {
+		arr := strings.Split(fileInfoList[i].Name(), "-")
+		for _ , val := range arr {
+				file.Write([]byte(string(val)))
+				file.Write([]byte{'\t'})
+
+		}
+		file.Write([]byte{'\n'})
+	}
 }
